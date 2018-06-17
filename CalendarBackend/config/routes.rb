5ad389d0c8f73523804c2bc984f1f990d7919f2e
Marvin Yan <marvin.yan@outlook.com>
2018-06-17 12:25:31 -0400

    Improve API endpoint URL with an 'api' subdomain and versioning

--- CalendarBackend/config/routes.rb
+++ CalendarBackend/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    resources :events, only: %i[index create update destroy]
+  constraints subdomain: "api" do
+    scope module: "api" do
+      namespace :v1, defaults: { format: :json } do
+        resources :events, only: %i[index create update destroy]
+      end
+    end
   end
 end

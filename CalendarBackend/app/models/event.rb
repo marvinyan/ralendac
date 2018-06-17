57cbf1288e110b33da42a38c09ecfd6cb83afd9b
Marvin Yan <marvin.yan@outlook.com>
2018-06-17 12:03:46 -0400

    Complete backend actions

--- CalendarBackend/app/models/event.rb
+++ CalendarBackend/app/models/event.rb
@@ -1,10 +1,17 @@
 class Event < ApplicationRecord
     validates :description, :start_time, :end_time, presence: true
     validate :end_time_cannot_be_before_start_time
+    validate :start_time_and_end_time_must_be_same_day
 
     def end_time_cannot_be_before_start_time
         if end_time < start_time
             errors.add(:end_time, "must be after start time")
+        end
+    end
+
+    def start_time_and_end_time_must_be_same_day
+        if start_time.beginning_of_day != end_time.beginning_of_day
+            errors.add(:end_time, "must occur on the same day as the start time")
         end
     end
 end
--- CalendarBackend/config/routes.rb
+++ CalendarBackend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resources :events, only: %i[index create show update destroy]
+    resources :events, only: %i[index create update destroy]
   end
 end

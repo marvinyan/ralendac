7635541292da14e3785192ab8ab71a185d2a6cd8
Marvin Yan <marvin.yan@outlook.com>
2018-06-18 18:39:55 -0400

    Restore end date after start date constraint

--- CalendarBackend/app/models/event.rb
+++ CalendarBackend/app/models/event.rb
@@ -1,17 +1,11 @@
 class Event < ApplicationRecord
     validates :description, :start_time, :end_time, presence: true
+    validate :end_time_cannot_be_before_start_time
     
     # Removed after realizing server time is ISO and app time can be any timezone.
     # Enforcing same day constraint server side would require knowing user's timezone.
 
-    # validate :end_time_cannot_be_before_start_time
     # validate :start_time_and_end_time_must_be_same_day
-
-    # def end_time_cannot_be_before_start_time
-    #     if valid_times? && end_time < start_time
-    #         errors.add(:end_time, "must be after start time")
-    #     end
-    # end
 
     # def start_time_and_end_time_must_be_same_day
     #     if valid_times? && start_time.beginning_of_day != end_time.beginning_of_day
@@ -19,9 +13,15 @@ class Event < ApplicationRecord
     #     end
     # end
 
-    # # Rails runs through ALL validations, even if the first one fails
-    # # When times are missing, only display "can't be blank" messages
-    # def valid_times?
-    #     return !start_time.nil? && !end_time.nil?
-    # end
+    def end_time_cannot_be_before_start_time
+        if valid_times? && end_time < start_time
+            errors.add(:end_time, "must be after start time")
+        end
+    end
+
+    # Rails runs through ALL validations, even if the first one fails
+    # When times are missing, only display "can't be blank" messages
+    def valid_times?
+        return !start_time.nil? && !end_time.nil?
+    end
 end

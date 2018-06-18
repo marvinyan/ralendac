5eaf9e4d6aee606ed3ad816815d827da211d700b
Marvin Yan <marvin.yan@outlook.com>
2018-06-18 17:42:44 -0400

    Remove server side time constraint for now

--- CalendarBackend/app/models/event.rb
+++ CalendarBackend/app/models/event.rb
@@ -1,23 +1,27 @@
 class Event < ApplicationRecord
     validates :description, :start_time, :end_time, presence: true
-    validate :end_time_cannot_be_before_start_time
-    validate :start_time_and_end_time_must_be_same_day
+    
+    # Removed after realizing server time is ISO and app time can be any timezone.
+    # Enforcing same day constraint server side would require knowing user's timezone.
 
-    def end_time_cannot_be_before_start_time
-        if valid_times? && end_time < start_time
-            errors.add(:end_time, "must be after start time")
-        end
-    end
+    # validate :end_time_cannot_be_before_start_time
+    # validate :start_time_and_end_time_must_be_same_day
 
-    def start_time_and_end_time_must_be_same_day
-        if valid_times? && start_time.beginning_of_day != end_time.beginning_of_day
-            errors.add(:end_time, "must occur on the same day as the start time")
-        end
-    end
+    # def end_time_cannot_be_before_start_time
+    #     if valid_times? && end_time < start_time
+    #         errors.add(:end_time, "must be after start time")
+    #     end
+    # end
 
-    # Rails runs through ALL validations, even if the first one fails
-    # When times are missing, only display "can't be blank" messages
-    def valid_times?
-        return !start_time.nil? && !end_time.nil?
-    end
+    # def start_time_and_end_time_must_be_same_day
+    #     if valid_times? && start_time.beginning_of_day != end_time.beginning_of_day
+    #         errors.add(:end_time, "must occur on the same day as the start time")
+    #     end
+    # end
+
+    # # Rails runs through ALL validations, even if the first one fails
+    # # When times are missing, only display "can't be blank" messages
+    # def valid_times?
+    #     return !start_time.nil? && !end_time.nil?
+    # end
 end

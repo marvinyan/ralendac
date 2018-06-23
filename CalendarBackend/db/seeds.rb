5db95d6a4c68fdeb4abcd28d8221a9083308f1bb
Marvin Yan <marvin.yan@outlook.com>
2018-06-22 22:05:34 -0400

    Add event seeding

--- CalendarBackend/db/seeds.rb
+++ CalendarBackend/db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Event.destroy_all
+
+year2018 = Date.new(2018, 1, 1)..Date.new(2018, 12, 31)
+zone = 'Eastern Time (US & Canada)'
+
+year2018.each do |date|
+    if rand() < 0.2
+        descrip_len = rand(1..5)
+        
+        Time.use_zone(zone) do
+            descrip_len.times do
+                start_of_day = Time.zone.parse(date.to_s)
+                end_of_day = start_of_day.end_of_day
+
+                random_time_1 = rand(start_of_day..end_of_day)
+                random_time_2 = rand(start_of_day..end_of_day)
+
+                start_time = [random_time_1, random_time_2].min.floor_to(15.minutes)
+                end_time = [random_time_1, random_time_2].max.floor_to(15.minutes)
+
+                description = Faker::Lorem.words(descrip_len, true).join(' ').capitalize
+                Event.create({ description: description, start_time: start_time, end_time: end_time})
+            end
+        end
+    end
+end

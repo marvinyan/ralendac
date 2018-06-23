20dde5989be04a12cd1aadd075158ec9312b35cb
Marvin Yan <marvin.yan@outlook.com>
2018-06-23 18:56:25 -0400

    Update seeds.rb

--- CalendarBackend/db/seeds.rb
+++ CalendarBackend/db/seeds.rb
@@ -1,15 +1,16 @@
 Event.destroy_all
 DatabaseCleaner.clean_with(:truncation)
 
-year2018 = Date.new(2018, 1, 1)..Date.new(2018, 12, 31)
+date_range = Date.new(2017, 1, 1)..Date.new(2019, 12, 31)
 zone = 'Eastern Time (US & Canada)'
 
-year2018.each do |date|
+date_range.each do |date|
     if rand() < 0.2
         descrip_len = rand(1..5)
-        
+        max_events = rand(1..6)
+
         Time.use_zone(zone) do
-            descrip_len.times do
+            max_events.times do
                 start_of_day = Time.zone.parse(date.to_s)
                 end_of_day = start_of_day.end_of_day
 
@@ -19,7 +20,7 @@ year2018.each do |date|
                 start_time = [random_time_1, random_time_2].min.floor_to(15.minutes)
                 end_time = [random_time_1, random_time_2].max.floor_to(15.minutes)
 
-                description = Faker::Lorem.words(descrip_len, true).join(' ').capitalize
+                description = Faker::Hipster.words(descrip_len, true).join(' ').capitalize
                 Event.create({ description: description, start_time: start_time, end_time: end_time})
             end
         end
